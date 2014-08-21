53bc841c5c0874c9e366795cfaf39d65f80fee8f
mffuser <Christian.Groll@stat.uni-muenchen.de>
2014-08-21 09:50:21 +0200

    data download function renamed

--- common_utilities/data_handling/processDataToReturns/getPrices.m
+++ common_utilities/data_handling/processDataToReturns/getPrices.m
@@ -1,10 +1,16 @@
-function joinedTable = processDataWithNaNs(dateBeg, dateEnd, tickerSymbs)
+function joinedTable = getPrices(dateBeg, dateEnd, tickerSymbs)
 %
 % Input:
 %   dateBeg     same format as for hist_stock_data
 %   dateEnd     same format as for hist_stock_data
 %   tickerSymbs     1xn cell array of ticker symbol strings
-
+%
+% Output:
+%   joinedTable     a mxn table of stock prices for multiple stocks, with
+%                   all dates that occur in at least one stock price series
+%                   and missing observations filled with NaNs. Dates are
+%                   sorted ascending and stored as strings in the RowNames
+%                   property of the table.
 
 % download data
 stockStructure = [];
