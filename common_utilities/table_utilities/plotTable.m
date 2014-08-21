8f1b585e3612545c960183704259d351a4373d6e
mffuser <Christian.Groll@stat.uni-muenchen.de>
2014-08-21 14:07:22 +0200

    plot multiple columns

--- common_utilities/table_utilities/plotTable.m
+++ common_utilities/table_utilities/plotTable.m
@@ -1,5 +1,5 @@
 function plotTable(tableToPlot)
 serialDates = datenum(tableToPlot.Properties.RowNames);
-plot(serialDates, tableToPlot{:, 1})
+plot(serialDates, tableToPlot{:, :})
 datetick 'x'
 end
