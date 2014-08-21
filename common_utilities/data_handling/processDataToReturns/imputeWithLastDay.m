36e616d19773064533b5f9534a67cb6b30edc9b3
mffuser <Christian.Groll@stat.uni-muenchen.de>
2014-08-21 14:06:31 +0200

    imputeWIth last day added

--- common_utilities/data_handling/processDataToReturns/imputeWithLastDay.m
+++ common_utilities/data_handling/processDataToReturns/imputeWithLastDay.m
@@ -1,5 +1,20 @@
-function imputeWithLastDay(values)
+function values = imputeWithLastDay(values)
+%
+% Inputs:
+%   values  nxm matrix
+%
+% Outputs:
+%   values  nxm matrix with imputed values
 
+% Replace NaN with observation of last day. 
+nRows = size(values, 2);
 
+% find missing values and replace with previous observation
+missingValues = isnan(values);
+
+nansToReplace = logical([zeros(1, nRows); missingValues(2:end, :)]);
+replaceWith = logical([missingValues(2:end, :); zeros(1, nRows)]);
+
+values(nansToReplace) = values(replaceWith);
 
 end
