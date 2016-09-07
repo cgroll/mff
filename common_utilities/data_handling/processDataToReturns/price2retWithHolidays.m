76c15828f1305e4da506a5437bc8a54a04886603
cgroll <groll.christian.edu@gmail.com>
2016-09-07 08:58:49 +0200

    return function updated

--- common_utilities/data_handling/processDataToReturns/price2retWithHolidays.m
+++ common_utilities/data_handling/processDataToReturns/price2retWithHolidays.m
@@ -1,11 +1,20 @@
-function retsTable = price2retWithHolidays(prices)
+function retsTable = price2retWithHolidays(prices, datesInFirstCol)
 %
 % Input:
-%   prices  nxm matrix or table of prices
+%   prices              nxm matrix or table of prices
+%   datesInFirstCol     boolean: is first column dates column
 %
 % Output:
 %   retsTable    (n-1)xm table of logarithmic returns
 
+if datesInFirstCol
+    % extract dates for later use
+    dates = prices(:, 1);
+    
+    % get only prices
+    prices = prices(:, 2:end);
+end
+    
 % get missing values
 missingValues = isnan(prices{:,:});
 
@@ -22,7 +31,11 @@ rets = diff(pricesImputed);
 % fill in NaNs again
 rets(missingValues(2:end, :)) = NaN;
 
-% embed returns in table meta-data
-retsTable = embed(rets, prices(2:end, :));
+% attach meta-data
+retsTable = array2table(rets, 'VariableNames', tabnames(prices));
+
+if datesInFirstCol
+    retsTable = [dates(2:end, :) retsTable];
+end
 
 end
